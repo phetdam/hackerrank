a46519db9cf7be1fe0766f829899bf6e1a98f9ed
Derek Huang <djh458@stern.nyu.edu>
2024-03-06 02:21:27 -0500

    add naive solution for array_manipulation + test cases
    data/array_manipulation/0.in
    data/array_manipulation/0.out
    data/array_manipulation/15.in
    data/array_manipulation/15.out
    data/array_manipulation/5.in
    data/array_manipulation/5.out
    data/array_manipulation/7.in
    data/array_manipulation/7.out
        Add test cases. Test case 7 will cause timeout
    
    src/CMakeLists.txt
        Add target and tests
    
    src/array_manipulation.cc
        Add naive solution. Optimal solution would be to merge intervals and update
        the interval value accordingly each time.

---
+++ src/array_manipulation.cc
@@ -0,0 +1,71 @@
+/**
+ * @file array_manipulation.cc
+ * @author Derek Huang
+ * @brief C++ submission for HackerRank problem "Array Manipulation"
+ * @copyright MIT License
+ *
+ * URL: https://www.hackerrank.com/challenges/crush/problem
+ *
+ * @note This naive solution times out on the larger test cases.
+ */
+
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+// only used when compiling as standalone test program
+#ifdef PDHKR_TEST
+#include <fstream>
+#include <sstream>
+
+#include "pdhkr/compare.hh"
+#include "pdhkr/testing.hh"
+#endif  // PDHKR_TEST
+
+int main()
+{
+// building as standalone test program
+#if defined(PDHKR_TEST)
+  // write to stringstream, read from PDHKR_TEST_INPUT
+  std::stringstream fout;
+  std::ifstream fin{PDHKR_TEST_INPUT};
+  // fans provides the expected output
+  std::ifstream fans{PDHKR_TEST_OUTPUT};
+#else
+  auto& fout = std::cout;
+  auto& fin = std::cin;
+#endif  // !defined(PDHKR_TEST)
+  // array size
+  unsigned int array_size;
+  fin >> array_size;
+  // number of operations
+  unsigned int n_ops;
+  fin >> n_ops;
+  // create array and perform operations. we need long long (64 bits) type
+  // since based on the problem constraints, max array value can be 2e14
+  std::vector<unsigned long long> values(array_size, 0UL);
+  for (decltype(n_ops) i = 0; i < n_ops; i++) {
+    // left index, right index, summand
+    unsigned int l_index, r_index, summand;
+    fin >> l_index;
+    fin >> r_index;
+    fin >> summand;
+    // apply to array
+    std::transform(
+      values.begin() + (l_index - 1),
+      values.begin() + r_index,
+      values.begin() + (l_index - 1),
+      [summand](const auto& v) { return v + summand; }
+    );
+  }
+  // write result
+  fout << *std::max_element(values.begin(), values.end());
+// if testing, do comparison in the program itself
+#if defined(PDHKR_TEST)
+  return pdhkr::exit_compare<decltype(values)::value_type>(fans, fout);
+#else
+  return EXIT_SUCCESS;
+#endif  // !defined(PDHKR_TEST)
+  return EXIT_SUCCESS;
+}
