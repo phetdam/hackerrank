66c0b17f56a020fc3aff1ab6c040226ceeaeeb37
Derek Huang <djh458@stern.nyu.edu>
2024-03-06 01:51:48 -0500

    rename pdhkr_add_tests.cmake to pdhkr_testing.cmake + add pdhkr_skip_tests
    CMakeLists.txt
        Update CMake module include
    
    cmake/pdhkr_add_tests.cmake -> cmake/pdhkr_testing.cmake
        Rename + add pdhkr_skip_tests for skipping compiled test cases
    
    src/CMakeLists.txt
        Skip test cases 2, 3 for subtrees_and_paths

--- cmake/pdhkr_testing.cmake
+++ cmake/pdhkr_testing.cmake
@@ -63,3 +63,40 @@ function(pdhkr_add_tests)
         add_test(NAME ${TEST_TARGET} COMMAND ${TEST_TARGET})
     endforeach()
 endfunction()
+
+##
+# Skip added tests for the given test cases for a HackerRank submission.
+#
+# Any CTest test that was already added with pdhkr_add_tests can be skipped
+# here, e.g. if the test takes too long to run, if the test is failing, etc.
+#
+# Arguments:
+#   TARGET target
+#       Name of the HackerRank submission target
+#
+#   TEST_CASES test_case...
+#       Test case identifiers, suffixed to the test program name after an
+#       underscore, that is also the stem for the relevant .in, .out files.
+#       See pdhkr_add_tests for details on the naming convention.
+#
+function(pdhkr_skip_tests)
+    # parse TARGET parent target name, TEST_CASES test target suffices + stems
+    # for the input/output files. ARGV contains all the function arguments
+    set(SINGLE_VALUE_ARGS TARGET)
+    set(MULTI_VALUE_ARGS TEST_CASES)
+    cmake_parse_arguments(
+        HOST
+        "" "${SINGLE_VALUE_ARGS}" "${MULTI_VALUE_ARGS}" ${ARGV}
+    )
+    # check args. both are required
+    if(NOT DEFINED HOST_TARGET)
+        message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION}: missing TARGET")
+    endif()
+    if(NOT DEFINED HOST_TEST_CASES)
+        message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION}: missing TEST_CASES")
+    endif()
+    # for each of the test case stems skip EXIT_FAILURE
+    foreach(CASE IN LISTS HOST_TEST_CASES)
+        set_tests_properties(${HOST_TARGET}_${CASE} PROPERTIES SKIP_RETURN_CODE 1)
+    endforeach()
+endfunction()
